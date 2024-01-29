292143f967eb93a9f57ed81b643e27f668a4cca5
SamuelSherpa2020 <samuelsherpa46@gmail.com>
2024-01-29 11:57:29 +0545

    Add-Change the servername database to avoid targeting server database.

--- ImageSlayer/Program.cs
+++ ImageSlayer/Program.cs
@@ -10,13 +10,12 @@ namespace ImageConverter
         static void Main(string[] args)
         {
 
-            //string serverName = "LAZARUS\\SQLEXPRESS";
-            string serverName = "103.94.159.2";
+            string serverName = "LAZARUS\\SQLEXPRESS";
+
             string databaseName = "Putalibazarmun";
             string connString = $"Server={serverName};Database={databaseName};user id=sa;password=Silicon321;Integrated Security=false;";
             //string outputPath = @"C:\Users\someo\OneDrive\PREV FILES\Documents\5-25-2023(Sifaris)";
-            //string outputPath = @"E:\Sifaris\Putalibazar\Sifaris\MunicipalRecommendation\wwwroot\FilesDebug\"; //permission check
-            string outputPath = @"C:\TrialImages\"; //permission check
+            string outputPath = @"E:\Sifaris\Putalibazar\Sifaris\MunicipalRecommendation\wwwroot\FilesDebug\"; //permission check
 
             int batchSize = 100; // Set your desired batch size
             //DataTable dt = new();
